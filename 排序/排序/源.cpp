0eb35c28b17a684dbe4feb2be23e5717b0661535
dianxiap <1207864396@qq.com>
2023-04-03 21:56:15 +0800

    排序

---
+++ 排序/排序/源.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+void Insertsort(int* a, int n)
+{
+	for (int i = 1; i < n; i++)
+	{
+		int end = i - 1;
+		int tmp = a[i];
+		while (end >= 0)
+		{
+			if (tmp < a[end])
+			{
+				a[end + 1] = a[end];
+				--end;
+			}
+			else
+			{
+				break;
+			}
+		}
+		a[end + 1] = tmp;
+	}
+}
+
+void ShellSort(int* a, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		gap /= 2;
+		for (int i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int tmp = a[i + gap];
+			while (end >= 0)
+			{
+				if (tmp < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+	}
+}
