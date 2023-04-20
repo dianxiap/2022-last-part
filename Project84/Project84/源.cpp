e252f967e17a7e85d56cbdc12e3df7447d2fa65b
dianxiap <1207864396@qq.com>
2023-04-20 19:43:42 +0800

    C++引用

---
+++ Project84/Project84/源.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
+using namespace std;
+//ÄÚÁªº¯Êý
+void f(int)
+{
+	cout << "f(int)" << endl;
+}
+void f(int*)
+{
+	cout << "f(int*)" << endl;
+}
+int main()
+{
+	f(0);
+	f(NULL);
+	f((int*)NULL);
+	return 0;
+}
